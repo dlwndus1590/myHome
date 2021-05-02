279c1ee97227688ce49265abb8af2a6870e2b3d6
jooyn <jooyn@print>
2021-05-02 16:52:15 +0900

    1. dto 작성 -> Category, Product 2. category.jsp, best.jsp 화면 ui 작성 3. inc > header.jsp에 url 추가

--- src/com/myHome/model/dto/Category.java
+++ src/com/myHome/model/dto/Category.java
@@ -1,5 +1,49 @@
 package com.myHome.model.dto;
 
-public class Category {
+import java.io.Serializable;
 
+/** 
+ * 카테고리 도메인 클래스
+ * @author 이주연
+ */
+public class Category implements Serializable {
+	/** 카테고리 번호 */
+	private int categoryId;
+	/** 카테고리 명 */
+	private String categoryName;
+	
+	/** 기본생성자 */
+	public Category() {}
+
+	/**
+	 * 생성자
+	 * @param categoryId
+	 * @param categoryName
+	 */
+	public Category(int categoryId, String categoryName) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+
+	/** getter/setter */
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [categoryId=" + categoryId + ", categoryName=" + categoryName + "]";
+	}	
 }
--- src/com/myHome/model/dto/Product.java
+++ src/com/myHome/model/dto/Product.java
@@ -1,5 +1,160 @@
 package com.myHome.model.dto;
 
-public class Product {
+import java.io.Serializable;
 
+/**
+ * 상품 도메인 클래스
+ * @author 이주연
+ */
+public class Product implements Serializable {
+	/** 상품 번호 */
+	private int pNo;
+	/** 상품명 */
+	private String pName;
+	/** 가격 */
+	private int pPrice;
+	/** 사진 */
+	private String pImg;
+	/** 상품설명 */
+	private String pDescribe;
+	/** 배송비 */
+	private int deliveryFee;
+	/** 회사/점포명 */
+	private String companyName;
+	/** 카테고리 번호 */
+	private int categoryId;
+	/** 평점 */
+	private int pScore;
+	/** 판매량 */
+	private int pSales;
+	/** 재고 */
+	private int pCount;
+	
+	/** 기본생성자 */
+	public Product() {}
+
+	/**
+	 * 생성자
+	 * @param pNo
+	 * @param pName
+	 * @param pPrice
+	 * @param pImg
+	 * @param pDescribe
+	 * @param deliveryFee
+	 * @param companyName
+	 * @param categoryId
+	 * @param pScore
+	 * @param pSales
+	 * @param pCount
+	 */
+	public Product(int pNo, String pName, int pPrice, String pImg, String pDescribe, int deliveryFee,
+			String companyName, int categoryId, int pScore, int pSales, int pCount) {
+		this.pNo = pNo;
+		this.pName = pName;
+		this.pPrice = pPrice;
+		this.pImg = pImg;
+		this.pDescribe = pDescribe;
+		this.deliveryFee = deliveryFee;
+		this.companyName = companyName;
+		this.categoryId = categoryId;
+		this.pScore = pScore;
+		this.pSales = pSales;
+		this.pCount = pCount;
+	}
+
+	/** getter/setter */
+	public int getpNo() {
+		return pNo;
+	}
+
+	public void setpNo(int pNo) {
+		this.pNo = pNo;
+	}
+
+	public String getpName() {
+		return pName;
+	}
+
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	public int getpPrice() {
+		return pPrice;
+	}
+
+	public void setpPrice(int pPrice) {
+		this.pPrice = pPrice;
+	}
+
+	public String getpImg() {
+		return pImg;
+	}
+
+	public void setpImg(String pImg) {
+		this.pImg = pImg;
+	}
+
+	public String getpDescribe() {
+		return pDescribe;
+	}
+
+	public void setpDescribe(String pDescribe) {
+		this.pDescribe = pDescribe;
+	}
+
+	public int getDeliveryFee() {
+		return deliveryFee;
+	}
+
+	public void setDeliveryFee(int deliveryFee) {
+		this.deliveryFee = deliveryFee;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public int getpScore() {
+		return pScore;
+	}
+
+	public void setpScore(int pScore) {
+		this.pScore = pScore;
+	}
+
+	public int getpSales() {
+		return pSales;
+	}
+
+	public void setpSales(int pSales) {
+		this.pSales = pSales;
+	}
+
+	public int getpCount() {
+		return pCount;
+	}
+
+	public void setpCount(int pCount) {
+		this.pCount = pCount;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [pNo=" + pNo + ", pName=" + pName + ", pPrice=" + pPrice + ", pImg=" + pImg + ", pDescribe="
+				+ pDescribe + ", deliveryFee=" + deliveryFee + ", companyName=" + companyName + ", categoryId="
+				+ categoryId + ", pScore=" + pScore + ", pSales=" + pSales + ", pCount=" + pCount + "]";
+	}
 }
