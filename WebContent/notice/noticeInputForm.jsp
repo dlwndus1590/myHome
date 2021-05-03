c0117ea6ccdbe5cf5651f09dfed4cbf86413a0fb
jspea <jspea@DESKTOP-KRI1Q7A>
2021-05-03 21:18:44 +0900

    공지사항 게시글 완료 및 질문 답변 게시판 UI 구현

---
+++ WebContent/notice/noticeInputForm.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>My Home</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="공지사항 게시글 작성">
+<meta name="author" content="김보성">
+<link href="${CONTEXT_PATH}/assets/css/bootstrap.css" rel="stylesheet" />
+<link href="${CONTEXT_PATH}/assets/style/style.css" rel="stylesheet" />
+<link href="${CONTEXT_PATH}/assets/font-awesome/css/font-awesome.css"
+	rel="stylesheet">
+<link href="${CONTEXT_PATH}/css/notice/noticeInputForm.css"
+	rel="stylesheet" />
+<link rel="shortcut icon" href="assets/ico/favicon.ico">
+</head>
+<body>
+	<!-- header -->
+	<jsp:include page="/inc/header.jsp" />
+
+	<!-- contents menu -->
+	<h3 align="center">공지사항 게시글 작성</h3>
+	<form action="${CONTEXT_PATH}/notice/noticeController?action=noticeInput" method="post">
+		<table border="1px" align="center" width="400px" cellpadding="5px">
+			<tr>
+				<td align="center" height="20px"><input type="text"
+					name="nTitle" placeholder="게시글 제목을 입력하세요." size="95px"
+					required="required" style="height: 30px" class="info"></td>
+			</tr>
+
+			<tr>
+				<td><textarea name="nContent" cols="100" rows="30"
+						placeholder="내용을 입력해주세요." required="required" class="info"></textarea></td>
+			</tr>
+			<tr align="right">
+				<td id ="buttonLine"><input type="submit" value="등록" size="100px" id="button">
+				</td>
+			</tr>
+		</table>
+	</form>
+
+
+	<!-- footer -->
+	<jsp:include page="/inc/footer.jsp" />
+</body>
+</html>
--- docs/myHome_dml.sql
+++ docs/myHome_dml.sql
@@ -14,11 +14,16 @@ insert into category values (3, '홈데코/조명');
 insert into category values (4, '수납/정리');
 insert into category values (5, '생활용품');
 insert into category values (6, '주방');
-insert into category values (7, '조명');
+insert into category values (7, 'DIY/공구');
 
--- product 초기화데이터
-insert into product values (1, '잠이 솔솔 오는 배개',  25000, empty_blob(), empty_blob(), 2500, 'KCC', 2, 4.5, 125, 200);
-insert into product values (2, '푹신푹신 매트리스 커버',  38000, empty_blob(), empty_blob(), 2500, 'KCC', 2, 4.6, 80, 120);
+insert into product values (1, '박나래 기절베개 - 호텔식베개 커버포함 2종',  25900, 'img/product/1.jpg', 'img/productDetail/1.png', 0, 'KCC', 2, 4.87, 4979 , 2000);
+insert into product values (2, '프란시스 Y3.3 커피머신 + 웰컴캡슐 14개',  119000, 'img/product/2.jpg', 'img/productDetail/2.png', 5000, 'KCC', 1, 4.9, 1632, 1000);
+insert into product values (3, '헤링본 발탄 사이잘룩 러그 카페트',  12800, 'img/product/3.jpg', 'img/productDetail/3.png', 0, 'KCC', 2, 4.85, 12614, 1000);
+insert into product values (4, '스마트전구 (LED 컬러 전구 / 앱연동)',  19900, 'img/product/4.jpg', 'img/productDetail/4.png', 3000, 'KCC', 3, 3.85, 8, 100);
+insert into product values (5, 'RASKOG 로스코그 키친 카트 트롤리 웨건',  34900, 'img/product/5.jpg', 'img/productDetail/5.png', 0, 'KCC', 4, 4.55, 1106, 1000);
+insert into product values (6, '비바체 100매 20팩 캡형 물티슈',  14900, 'img/product/6.jpg', 'img/productDetail/6.png', 0, 'KCC', 5, 4.78, 1178, 1000);
+insert into product values (7, '놋담 달수저 외 인기 커트러리 모음',  25800, 'img/product/7.jpg', 'img/productDetail/7.png', 2500, 'KCC', 6, 4.45, 1086, 1000);
+insert into product values (8, '3.7V 전동 드라이버 WW-D1',  29900, 'img/product/8.jpg', 'img/productDetail/8.png', 0, 'KCC', 7, 4.3, 184, 300);
 
 -- cart 초기화데이터
 insert into cart values (1, 'user01', 2, 2);
@@ -27,7 +32,18 @@ insert into cart values (1, 'user01', 2, 2);
 insert into review values (1, 2, '배개 후기입니다.', empty_blob(), '후기내용 어쩌고저쩌고', 4.8, 'user01', '2021-04-02');
 
 -- qnotice 초기화데이터
-insert into qnotice values (1, '이거 어떻게 해결해야하나요', '답변 부탁드립니다.', empty_blob(), 'user01', to_char(sysdate, 'yyyy-mm-dd'), 5);
+insert into qnotice values (1, '이거 어떻게 해결해야하나요', '답변 부탁드립니다.', '/img/interior.jpg', 'user01', to_char(sysdate, 'yyyy-mm-dd'), 5);
+
+-- qnotice 초기화데이터
+insert into qnotice values (2, '마루바닥 눌림 어떻게 하면 좋을까요?', '사진과 같이 철제 다리로 된 소파를 사용한 지 6개월 정도 됐습니다.
+
+첫 3개월은 신경도 안썼는데 어느날 바닥을 보니 사진과 같이 마루바닥에 자국이 나 있더군요 ㅠㅠ
+
+다이소에서 여러 스크래치 방지 제품과 다리 보호 제품 사용해보다가 테프론 재질 스티커가 그나마 조금 덜 패이는 것 같아서 일단은 사용중입니다.
+
+그치만 자국이 남기는 남아서 계속 방법을 찾고 있는데, 혹시 테니스공을 쓰거나 러그를 깔면 마루에 자국이 안 남을까요? 아니면 사진과 같은 나무 원판을 다리 밑에 놓아서 무게를 분산시키는게 나을까요?
+
+그리고 눌린 부분을 다리미로 열을 가해 원상복구 시킬 수도 있을까요?', '/img/qNotice/maru.PNG', 'user01', to_char(sysdate, 'yyyy-mm-dd'), 5);
 
 -- answer 초기화데이터
 insert into answer values (1, 1, '그건 이렇게 해결하는겁니다.', 'ucamp01', to_char(sysdate, 'yyyy-mm-dd'));
@@ -38,10 +54,10 @@ insert into orders_method values (2, '신용카드');
 insert into orders_method values (3, '휴대폰결제');
 
 -- orders_detail 초기화데이터
-insert into orders_detail values (1, 2, 2);
+INSERT INTO orders_detail VALUES (1, 2, 2, 1);
 
 -- orders 초기화데이터
-insert into orders values (1, 'user01', 2, 2, 50000, to_char(sysdate, 'yyyy-mm-dd'), 2500);
+INSERT INTO orders VALUES (1, 'user01', 2, 50000, to_char(sysdate, 'yyyy-mm-dd'), 2500);
 
 -- interior 초기화데이터
 insert into interior values (1, '유캠프인테리어', 5, '설명 대체', '서울시 태해란로 11길 5');
